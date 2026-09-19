75d815751e8ea7b9237d42935e721e4c9c1f0f18
Tanja Gesslbauer <tanja.gesslbauer@stud.hcw.ac.at>
2026-09-19 18:08:31 +0200

    Add color coded markers per sample type

--- create-figure-interactive.py
+++ create-figure-interactive.py
@@ -9,8 +9,8 @@ center_lateral = data["lat"].mean()
 center_longitudinal = data["lng"].mean()
 
 m = folium.Map(
-	location=[center_lateral, center_longitudinal],
-	zoom_start=3
+    location=[center_lateral, center_longitudinal],
+    zoom_start=3
 )
 
 # feature group per sample type - actual filtering mechanism
@@ -19,7 +19,17 @@ sample_types = data["sampletype_label"].unique()
 
 layers = {}
 for stype in sample_types:
-	layers[stype] = folium.FeatureGroup(name=stype)
+    layers[stype] = folium.FeatureGroup(name=stype)
+
+
+#optional add on -color couding
+available_colors = ["blue", "green", "purple", "orange", "red", "darkred",
+                     "cadetblue", "darkgreen", "darkblue", "pink"]
+
+color_map = {}
+for i, stype in enumerate(sample_types):
+    color_map[stype] = available_colors[i]
+
 
 #add markers to their layer
 for _, row in data.iterrows():
@@ -32,10 +42,13 @@ Sample date: {row['timepoint'].split('T')[0]}
 <br>
 <a href={row["np"]}>See details ...</a>
 """
+#added color coding here
+    marker_color = color_map[row["sampletype_label"]]
     marker = folium.Marker(
-        location=[row["lat"], row["lng"]],
-        popup=popup_text
-    )
+            location=[row["lat"], row["lng"]],
+            popup=popup_text,
+            icon=folium.Icon(color=marker_color)
+        )
     marker.add_to(layers[row["sampletype_label"]])
 
 # add toggle
